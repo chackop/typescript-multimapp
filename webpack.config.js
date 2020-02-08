f9bb0f38e9d42e8460a4ca1174f3eb8a6f8db18a
Chacko <chackop@gmail.com>
2020-02-08 17:30:46 +0000

    setup ts

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,6 @@
 module.exports = {
   entry: './app/app.ts',
   devtool: 'inline-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      }
-    ]
-  },
   resolve: {
     extensions: [ '.tsx', '.ts', '.js' ]
   },
